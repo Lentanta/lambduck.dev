483c904126b07c9d7211e7754a539deaf7076b0e
Thanh tam <lentanta@pop-os.localdomain>
2023-06-30 21:06:05 +0700

    Remove all the unused code and packages

--- components/ColorDotAnimation/utils/functions.ts
+++
@@ -1,8 +0,0 @@
-export const randomBetween = (min: number, max: number) => {
-  return Math.floor(Math.random() * max) + min;
-};
-
-export const randomValueIn = <T>(listOfAny: T[]): T => {
-  const randomNumber = randomBetween(0, listOfAny.length);
-  return listOfAny[randomNumber];
-};
