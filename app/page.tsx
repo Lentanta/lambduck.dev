85fb4fb22bbe88ef8114271a9946e0cc1ce5e47c
Thanh tam <lentanta@pop-os.localdomain>
2024-08-18 16:06:56 +0700

    Update new UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Metadata } from 'next';
 import { Fragment } from 'react';
-import { Contact } from './Contact';
-import { AboutMe } from "./AboutMe";
+import { LinkButton } from './components/LinkButton';
 
 export const metadata: Metadata = {
   title: 'Lentanta website',
@@ -11,8 +10,41 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <Fragment>
-      <AboutMe />
-      <Contact />
+      <div className="flex flex-col items-center h-[500px] w-full bg-dark-bg col-span-full rounded-lg">
+        <div className="p-5 bg-dark-bg h-full rounded-lg flex flex-col justify-center items-center">
+          <p className="text-subTitle ml-1 font-bold text-dark-fg0">Welcome to my website</p>
+          <div className='rounded-lg mt-1 border-2 py-5 px-9 border-dark-blue2'>
+            <h1 className="text-8xl font-bold text-dark-blue2">Lambduck_</h1>
+          </div>
+          <p className="text-xl mt-2 ml-1 font-semibold text-dark-fg0">Lambda + Duck</p>
+        </div>
+      </div>
+
+      <div className="bg-dark-bg col-span-3 rounded-lg p-4">
+        <p className="text-xl text-dark-fg1">Information about this website:</p>
+        <p className="text-lg text-dark-fg1">I have experience as a software engineer and am passionate about game development and computer science. I enjoy creating fun, quirky software projects in my spare time and also like to draw pixel art.</p>
+      </div>
+
+      <LinkButton
+        target="_blank"
+        href="https://www.github.com/Lentanta"
+        className="bg-dark-bg3"
+        icon={<i className="ri-github-fill text-dark-fg0 text-7xl" />}
+      />
+
+      <LinkButton
+        target="_blank"
+        href="mailto: thanhtam180298@gmail.com"
+        className="bg-dark-red2"
+        icon={<i className="ri-mail-fill text-dark-fg0 text-7xl" />}
+      />
+
+      <LinkButton
+        target="_blank"
+        href="https://www.linkedin.com/in/thanh-t%C3%A2m-l%C3%AA-b95878193/"
+        className="bg-dark-blue1"
+        icon={<i className="ri-linkedin-fill text-dark-fg0 text-7xl" />}
+      />
     </Fragment>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,24 @@ module.exports = {
       },
       colors: {
         "color-dark": "#000000",
-        "color-light": "#FFFFFF"
+        "color-light": "#FFFFFF",
+        dark: {
+          bg: "#282828",
+          bg0_h: "#1d2021",
+          bg1: "#3C3836",
+          bg2: "#504945",
+          bg3: "#665c54",
+
+          fg0: "#fbf1c7",
+          fg1: "#ebdbb2",
+
+          blue1: "#458588",
+          blue2: "#83a598",
+          red1: "#cc241d",
+          red2: "#fb4934",
+          yellow1: "#d79921",
+          yellow2: "#fabd2f"
+        },
       },
       fontSize: {
         title: [
